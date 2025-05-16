fc45e2dbb840b8f6e2c3475bc04a6ea31ed4026d
Trần Hải Đông <Tranhaidongpk@gmail.com>
2025-05-16 14:45:21 +0700

    add script weapon

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity);
+
+        Debug.Log(hit.collider.name);
+    }
+}
