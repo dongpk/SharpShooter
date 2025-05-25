c37c21afa0aa211d8c30678fbb5e26865dfc680d
Trần Hải Đông <Tranhaidongpk@gmail.com>
2025-05-25 15:49:08 +0700

    add audiomanager

---
+++ Assets/Scripts/Misc/AudioManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+     public static AudioManager Instance { get; private set; }
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("Audio Clips")]
+    public AudioClip backgroundMusic;
+   
+    public AudioClip explosion;
+    public AudioClip gate;
+   
+
+    void Awake()
+    {
+       
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        if (backgroundMusic) PlayMusic(backgroundMusic, true);
+        else Debug.LogWarning("No background music assigned in AudioManager.");
+    }
+    public void PlayMusic(AudioClip clip, bool loop = true)
+    {
+        if (!clip) return;
+        musicSource.clip = clip;
+        musicSource.loop = loop;
+        musicSource.ignoreListenerPause = true;   // B3 – play khi Game paused
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        if (clip) sfxSource.PlayOneShot(clip);
+    }
+}
